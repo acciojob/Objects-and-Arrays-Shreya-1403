8d3d433376f13f94314c8b7ae6bd4c5d1a6cd911
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-07-27 18:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ let team1=[];
 for(let i=0;i<players.length;i++){
     team1[i]=players[i];
 }
-let cap1=person;
+let cap1={};
+for(x in person){
+    cap1.name=person.name;
+    cap1.age=person.age;
+}
 
 window.players = players;
 window.person = person;
